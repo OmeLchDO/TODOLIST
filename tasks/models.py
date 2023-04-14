96cfcb05abbb158a763b4870186a14b53759e5b3
itOmelchDO <it.omelchenko@gmail.com>
2023-04-14 23:09:26 +0300

    'Created models & added first migration'

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField()
+    deadline = models.DateTimeField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ["is_done", "datetime"]
