01115d317566f29cab5a78b58de72b76d75dd6f8
itOmelchDO <it.omelchenko@gmail.com>
2023-04-15 00:28:35 +0300

    'Fixed bootstrap5 to installed apps'

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "tasks",
-    "crispy_forms"
-    "bootstrap5"
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'tasks',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
