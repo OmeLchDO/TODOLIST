5fd184687a9b43c3b9578beec43adc72c008b326
itOmelchDO <it.omelchenko@gmail.com>
2023-04-15 00:29:01 +0300

    'Added tags page & solution'

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,13 +1,70 @@
 from django.urls import path
 
-from tasks.views import TaskListView, TaskCreateView
+from tasks.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    change_task
+)
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="TaskListView"),
+    path(
+        "",
+        TaskListView.as_view(),
+        name="TaskListView"
+    ),
+
     path(
         "tasks/create/",
         TaskCreateView.as_view(),
         name="task-create",
+    ),
+
+    path(
+        "tasks/<int:pk>/update/",
+        TaskUpdateView.as_view(),
+        name="task-update"
+    ),
+
+    path(
+        "tasks/<int:pk>/delete/",
+        TaskDeleteView.as_view(),
+        name="task-delete"
+    ),
+
+    path(
+        "task-status/<int:pk>/",
+        change_task,
+        name="change-task"
+    ),
+
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="TagListView"
+    ),
+
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create",
+    ),
+
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update"
+    ),
+
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete"
     ),
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from tasks.models import Task
+from tasks.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
@@ -15,3 +16,52 @@ class TaskCreateView(generic.CreateView):
     fields = "__all__"
     success_url = reverse_lazy("tasks:TaskListView")
     template_name = "tasks/task_form.html"
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:TaskListView")
+    template_name = "tasks/task_form.html"
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:TaskListView")
+    template_name = "tasks/task_form_confirm_delete.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "tasks/tag_list.html"
+    context_object_name = "tags_list"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:TagListView")
+    template_name = "tasks/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:TagListView")
+    template_name = "tasks/tag_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tasks:TagListView")
+    template_name = "tasks/tag_form_confirm_delete.html"
+
+
+def change_task(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.is_done is True:
+        task.is_done = False
+    else:
+        task.is_done = True
+    task.save()
+    return HttpResponseRedirect(reverse_lazy("tasks:TaskListView"))
